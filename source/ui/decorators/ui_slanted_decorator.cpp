9b970b907b152bd260ffc9827ea2438261dbf728
dnk777 <dnk777@users.noreply.github.com>
2018-05-12 23:27:08 +0300

    UI: Draw slanted decorator sides in software
    It is nearly impossible to match the hardware
    drawn inclined line, especially given various
    display density/multisampling settings.

--- source/ui/decorators/ui_slanted_decorator.cpp
+++ source/ui/decorators/ui_slanted_decorator.cpp
@@ -12,10 +12,14 @@ typedef Rocket::Core::PropertyDictionary PropertyDictionary;
 typedef Rocket::Core::Colourb Colourb;
 typedef Rocket::Core::RenderInterface RenderInterface;
 typedef Rocket::Core::TextureHandle TextureHandle;
+typedef Rocket::Core::Vertex Vertex;
+typedef Rocket::Core::Vector2f Vector2f;
 
 class AntiAliasingBitmapCache {
 	struct CacheEntry {
-		TextureHandle handle;
+		// We might think about storing both sides data in a single texture
+		// but we have to fight (sub)pixel offsets due to rounding in this case
+		TextureHandle handles[2];
 		int numRefs;
 	};
 
@@ -23,14 +27,14 @@ class AntiAliasingBitmapCache {
 	RenderInterface *renderInterface;
 
 	static inline uint64_t MakeKey( const Vector2i &dimensions, float slantAngle );
-	static void FillTexture( Colourb *data, const Vector2i &dimensions );
+	static void FillTextures( Colourb *data, const Vector2i &dimensions );
 public:
 	AntiAliasingBitmapCache() : renderInterface( nullptr ) {}
 
 	~AntiAliasingBitmapCache();
 
-	TextureHandle GetTexture( const Vector2i &dimensions, float slantAngle, RenderInterface *renderInterface_ );
-	void ReleaseTexture( const Vector2i &dimensions, float slantAngle );
+	TextureHandle *GetTextures( const Vector2i &dimensions, float slantAngle, RenderInterface *renderInterface_ );
+	void ReleaseTextures( const Vector2i &dimensions, float slantAngle );
 };
 
 static AntiAliasingBitmapCache *antiAliasingBitmapCache = nullptr;
@@ -40,7 +44,7 @@ static AntiAliasingBitmapCache *antiAliasingBitmapCache = nullptr;
 
         slant-decorator: slanted;
         slant-color: #00FF00;
-        slant-angle: +45;
+        slant-angle: 45;
 */
 class SlantedDecorator final: public Decorator {
 	Colourb color;
@@ -59,14 +63,19 @@ public:
 			return;
 		} else {
 			// Values outside this range do not really make sense
-			clamp( angle, -60.0f, 60.0f );
+			clamp( angle, 0.0f, 45.0f );
 			tangent = ::tanf( DEG2RAD( angle ) );
 		}
 	}
 
 	struct BitmapElementData {
-		TextureHandle textureHandle;
+		TextureHandle textureHandles[2];
 		Vector2i bitmapDimensions;
+
+		BitmapElementData( TextureHandle *textureHandles_, const Vector2i &bitmapDimensions_ )
+			: bitmapDimensions( bitmapDimensions_ ) {
+			Vector2Copy( textureHandles_, textureHandles );
+		}
 	};
 
 	DecoratorDataHandle GenerateElementData( Element *element ) override {
@@ -87,15 +96,12 @@ public:
 			return (DecoratorDataHandle)0;
 		}
 
-		TextureHandle handle = WSWUI::antiAliasingBitmapCache->GetTexture( dimensions, angle, renderInterface );
-		if( !handle ) {
+		TextureHandle *handles = WSWUI::antiAliasingBitmapCache->GetTextures( dimensions, angle, renderInterface );
+		if( !handles ) {
 			return (DecoratorDataHandle)0;
 		}
 
-		auto *data = __new__( BitmapElementData );
-		data->bitmapDimensions = dimensions;
-		data->textureHandle = handle;
-		return (DecoratorDataHandle)data;
+		return (DecoratorDataHandle)( __new__( BitmapElementData )( handles, dimensions ) );
 	}
 
 	void ReleaseElementData( DecoratorDataHandle element_data ) override {
@@ -105,9 +111,14 @@ public:
 	}
 
 	void RenderElement( Element *element, DecoratorDataHandle element_data ) override {
-		typedef Rocket::Core::Vertex Vertex;
-		typedef Rocket::Core::Vector2f Vector2f;
+		if( auto *bitmapElementData = (BitmapElementData *)element_data ) {
+			RenderUsingAABitmap( element, bitmapElementData );
+		} else {
+			RenderUsingRawGeometry( element );
+		}
+	}
 
+	void RenderUsingRawGeometry( Element *element ) {
 		const float elementWidth = element->GetClientWidth();
 		const float elementHeight = element->GetClientHeight();
 
@@ -116,11 +127,49 @@ public:
 
 		Vector2f bottomRight = Vector2f( topLeft.x + elementWidth, topLeft.y + elementHeight );
 
-		// create the renderable vertexes
 		Vertex vertices[4];
+
+		const float shift = 0.5f * elementHeight * tangent;
 
 		vertices[0].colour = this->color;
 		vertices[0].tex_coord = Vector2f( 0.0f, 1.0f );
+		vertices[0].position = topLeft;
+		vertices[0].position.x += shift;
+
+		vertices[1].position = Vector2f( bottomRight.x + shift, topLeft.y );
+		vertices[1].tex_coord = Vector2f( 1.0f, 1.0f );
+		vertices[1].colour = this->color;
+
+		vertices[2].position = bottomRight;
+		vertices[2].position.x -= shift;
+		vertices[2].tex_coord = Vector2f( 1.0f, 0.0f );
+		vertices[2].colour = this->color;
+
+		vertices[3].position = Vector2f( topLeft.x - shift, bottomRight.y );
+		vertices[3].tex_coord = Vector2f( 0.0f, 0.0f );
+		vertices[3].colour = this->color;
+
+		int indices[6] = { 0, 1, 2, 0, 2, 3 };
+		Rocket::Core::RenderInterface *renderer = element->GetRenderInterface();
+
+		renderer->RenderGeometry( vertices, 4, indices, 6, 0, Vector2f( 0.0, 0.0 ) );
+	}
+
+	void RenderUsingAABitmap( Element *element, BitmapElementData *elementData ) {
+		const float elementWidth = element->GetClientWidth();
+		const float elementHeight = element->GetClientHeight();
+
+		Vector2f topLeft = Vector2f( element->GetAbsoluteLeft() + element->GetClientLeft(),
+									 element->GetAbsoluteTop() + element->GetClientTop() );
+
+		Vector2f bottomRight = Vector2f( topLeft.x + elementWidth, topLeft.y + elementHeight );
+
+		const float shift = 0.5f * elementHeight * tangent;
+
+		Vertex vertices[4];
+
+		vertices[0].tex_coord = Vector2f( 0.0f, 1.0f );
+		vertices[0].colour = this->color;
 
 		vertices[1].tex_coord = Vector2f( 1.0f, 1.0f );
 		vertices[1].colour = this->color;
@@ -131,63 +180,21 @@ public:
 		vertices[3].tex_coord = Vector2f( 0.0f, 0.0f );
 		vertices[3].colour = this->color;
 
-		const float shift = 0.5f * elementHeight * tangent;
+		vertices[0].position = Vector2f( topLeft.x + shift, topLeft.y );
+		vertices[1].position = Vector2f( bottomRight.x - shift, topLeft.y );
+		vertices[2].position = Vector2f( bottomRight.x - shift, bottomRight.y );
+		vertices[3].position = Vector2f( topLeft.x + shift, bottomRight.y );
 
 		int indices[6] = { 0, 1, 2, 0, 2, 3 };
-
 		Rocket::Core::RenderInterface *renderer = element->GetRenderInterface();
-
-		auto *bitmapElementData = (BitmapElementData *)element_data;
-		// Draw anti-aliasing bitmaps first (if they're present).
-		if( bitmapElementData ) {
-			// Setup vertices for the bitmap rectangle shifted to the left element side.
-			// Handle slant shift via renderer translations.
-
-			float side = +1.0f;
-			if( tangent < 0 ) {
-				// Flip X tex coords for negative degrees.
-				// Tex coords do not matter for the main geometry drawing anyway.
-				// This is not very nice but nobody is going to use negative angles.
-				// Note: The tex coords are also always flipped in Y axis
-				// to simplify implementation of Wu algorithm.
-				for( Vertex &v: vertices ) {
-					v.tex_coord.x = 1.0f - v.tex_coord.x;
-				}
-				side = -1.0f;
-			}
-
-			// Top left
-			vertices[0].position = topLeft;
-
-			// Top right
-			vertices[1].position = topLeft;
-			vertices[1].position.x += bitmapElementData->bitmapDimensions.x;
-
-			// Bottom right
-			vertices[2].position = topLeft;
-			vertices[2].position.x += bitmapElementData->bitmapDimensions.x;
-			vertices[2].position.y += bitmapElementData->bitmapDimensions.y;
-
-			// Bottom left
-			vertices[3].position = topLeft;
-			vertices[3].position.y += bitmapElementData->bitmapDimensions.y;
-
-			Vector2f leftTranslation( int( -side * ( shift + 0.5f ) ), 0 );
-			renderer->RenderGeometry( vertices, 4, indices, 6, bitmapElementData->textureHandle, leftTranslation );
-			Vector2f rightTranslation( elementWidth - int( side * ( shift + 0.5f ) ), 0 );
-			renderer->RenderGeometry( vertices, 4, indices, 6, bitmapElementData->textureHandle, rightTranslation );
-		}
-
-		vertices[0].position = topLeft;
-		vertices[0].position.x += shift;
-
-		vertices[1].position = Vector2f( bottomRight.x + shift, topLeft.y );
-
-		vertices[2].position = bottomRight;
-		vertices[2].position.x -= shift;
-
-		vertices[3].position = Vector2f( topLeft.x - shift, bottomRight.y );
 		renderer->RenderGeometry( vertices, 4, indices, 6, 0, Vector2f( 0.0, 0.0 ) );
+
+		vertices[0].position = Vector2f( topLeft.x - shift, topLeft.y );
+		vertices[1].position = Vector2f( topLeft.x + shift, topLeft.y );
+		vertices[2].position = Vector2f( topLeft.x + shift, bottomRight.y );
+		vertices[3].position = Vector2f( topLeft.x - shift, bottomRight.y );
+		renderer->RenderGeometry( vertices, 4, indices, 6, elementData->textureHandles[0], Vector2f( 0.0f, 0.0f ) );
+		renderer->RenderGeometry( vertices, 4, indices, 6, elementData->textureHandles[1], Vector2f( elementWidth, 0.0f ) );
 	}
 };
 
@@ -227,107 +234,126 @@ inline uint64_t AntiAliasingBitmapCache::MakeKey( const Vector2i &dimensions, fl
 	assert( dimensions.y > 0 && dimensions.y < ( 1 << 16 ) );
 	uint64_t hiPart = (uint64_t)( ( dimensions.x << 16 ) | dimensions.y );
 	uint64_t loPart = 0;
-	*( (float *)&loPart ) = fabsf( slantAngle );
+	assert( slantAngle >= 0 );
+	*( (float *)&loPart ) = slantAngle;
 	return ( hiPart << 32 ) | loPart;
 }
 
-static inline void PlotVaryingX( Colourb *data, int rowShift, float x, int y ) {
-	int ix = (int)x;
-	uint8_t alpha = (uint8_t)( 255 * ( x - ix ) );
-	data[ix + y * rowShift] = Colourb( 255, 255, 255, 255 - alpha );
-	data[ix + 1 + y * rowShift] = Colourb( 255, 255, 255, alpha );
-}
+static void ScanRightLine( Colourb *data, int rowShift, float x, int y ) {
+	const int ix = (int)x;
+	ptrdiff_t rowDataOffset = y * rowShift;
 
-static inline void PlotVaryingY( Colourb *data, int rowShift, int x, float y ) {
-	int iy = (int)y;
-	uint8_t alpha = (uint8_t)( 255 * ( y - iy ) );
-	data[x + iy * rowShift] = Colourb( 255, 255, 255 - alpha );
-	data[x + (iy + 1) * rowShift] = Colourb( 255, 255, 255, alpha );
-}
-
-void AntiAliasingBitmapCache::FillTexture( Colourb *data, const Vector2i &dimensions ) {
-	size_t numPixels = dimensions.x * dimensions.y;
-	memset( data, 0, sizeof( Colourb ) * numPixels );
-
-	float delta;
-	int rowShift = dimensions.x;
-	if( abs( dimensions.y ) > abs( dimensions.x ) ) {
-		delta = dimensions.x / (float)dimensions.y;
-		float x = delta;
-		PlotVaryingX( data, rowShift, x, 0 );
-		for( int y = 1; y < dimensions.y - 1; ++y ) {
-			PlotVaryingX( data, rowShift, x, y );
-			x += delta;
-		}
-		PlotVaryingX( data, rowShift, x, dimensions.y - 1 );
-		data[dimensions.x * ( dimensions.y - 1 )] = Colourb( 0, 0, 0, 0 );
-	} else {
-		delta = dimensions.y / (float)dimensions.x;
-		float y = delta;
-		PlotVaryingY( data, rowShift, 0, y );
-		for( int x = 1; x < dimensions.x - 1; ++x ) {
-			PlotVaryingY( data, rowShift, x, y );
-			y += delta;
-		}
-		PlotVaryingY( data, rowShift, dimensions.x - 1, y );
-		data[dimensions.x] = Colourb( 0, 0, 0, 0 );
+	for( int i = 0; i < ix + 1; ++i ) {
+		data[i + rowDataOffset] = Colourb( 255, 255, 255, 255 );
 	}
+
+	data[ix + 1 + rowDataOffset] = Colourb( 255, 255, 255, (uint8_t)( 255 * ( x - ix ) ) );
+}
+
+static void ScanLeftLine( Colourb *data, int rowShift, float x, int y ) {
+	const int ix = (int)x;
+	ptrdiff_t rowDataOffset = y * rowShift;
+
+	data[ix + rowDataOffset] = Colourb( 255, 255, 255, (uint8_t)( 255 - 255 * ( x - ix ) ) );
+	for( int i = ix + 1; i < rowShift; ++i ) {
+		data[i + rowDataOffset] = Colourb( 255, 255, 255, 255 );
+	}
+}
+
+void AntiAliasingBitmapCache::FillTextures( Colourb *data, const Vector2i &dimensions ) {
+	// Dimensions are supplied for a single side, both sides are combined in a single texture
+	size_t numPixels = ( dimensions.x + 1 ) * ( dimensions.y + 1 ) * 2u;
+	memset( data, 0, sizeof( Colourb ) * numPixels );
+	Colourb *left = data;
+	Colourb *right = data + numPixels / 2;
+
+	// We do not support negative / > 45 degrees angles anymore
+	assert( dimensions.x <= dimensions.y );
+
+	// A derivation of Wu algorithm.
+	// Not only plot two pixels with varied opacity,
+	// but fill a scanned line segment and plot a single pixel then.
+
+	int rowShift = dimensions.x;
+	float delta = dimensions.x / (float)dimensions.y;
+	float x = delta;
+	ScanLeftLine( left, rowShift, x, 0 );
+	ScanRightLine( right, rowShift, x, 0 );
+	for( int y = 1; y < dimensions.y - 1; ++y ) {
+		ScanLeftLine( left, rowShift, x, y );
+		ScanRightLine( right, rowShift, x, y );
+		x += delta;
+	}
+	ScanLeftLine( left, rowShift, x, dimensions.y - 1 );
+	ScanRightLine( right, rowShift, x, dimensions.y - 1 );
 }
 
 AntiAliasingBitmapCache::~AntiAliasingBitmapCache() {
 	// Accessing this->renderInterface in the loop is valid (if it is null there were no entries)
 	for( auto &entry: entries ) {
-		renderInterface->ReleaseTexture( entry.second.handle );
+		for( int i = 0; i < 2; ++i ) {
+			renderInterface->ReleaseTexture( entry.second.handles[i] );
+		}
 	}
 }
 
-TextureHandle AntiAliasingBitmapCache::GetTexture( const Vector2i &dimensions,
-												   float slantAngle,
-												   RenderInterface *renderInterface_ ) {
+TextureHandle *AntiAliasingBitmapCache::GetTextures( const Vector2i &dimensions,
+													 float slantAngle,
+													 RenderInterface *renderInterface_ ) {
 	// Save the render interface for further operations
 	this->renderInterface = renderInterface_;
 
 	// Check for the degenerate case
 	assert( dimensions.x * dimensions.y != 0 );
+	// Check sloppiness
+	assert( abs( dimensions.x ) <= abs( dimensions.y ) );
 
 	const uint64_t key = MakeKey( dimensions, slantAngle );
 	auto iterator = entries.find( key );
 	if( iterator != entries.end() ) {
 		( *iterator ).second.numRefs++;
-		return ( *iterator ).second.handle;
+		return ( *iterator ).second.handles;
 	}
 
-	auto handle = (TextureHandle)0;
+	TextureHandle handles[2] = { 0, 0 };
 
 	// Make sure we can legally access pixels that are +1 X/Y out of dimensions bounds
-	auto *data = (Colourb *)malloc( sizeof( Colourb ) * ( dimensions.x + 1 ) * ( dimensions.y + 1 ) );
+	auto *buffer = (Colourb *)malloc( 2 * sizeof( Colourb ) * ( dimensions.x + 1 ) * ( dimensions.y + 1 ) );
 	// Data nullity depends of OS memory over-commit settings... we did our best
-	if( data ) {
-		FillTexture( data, dimensions );
+	if( buffer ) {
+		FillTextures( buffer, dimensions );
 		// Try generating texture
-		if( !renderInterface_->GenerateTexture( handle, (Rocket::Core::byte *)data, dimensions, 4 ) ) {
-			// Ensure the handle is zero in case of failure
-			handle = (TextureHandle)0;
+		for( int i = 0; i < 2; ++i ) {
+			Colourb *textureData = buffer + i * ( dimensions.x + 1 ) * ( dimensions.y + 1 );
+			if( !renderInterface_->GenerateTexture( handles[i], (Rocket::Core::byte *)textureData, dimensions, 4 ) ) {
+				for( int j = 0; j < i; ++j ) {
+					renderInterface_->ReleaseTexture( handles[j] );
+				}
+				// Ensure handles are zero in this case
+				handles[0] = handles[1] = 0;
+			}
 		}
-		free( data );
+		free( buffer );
 	}
 
 	// Even if the texture creation has failed, add an entry, otherwise an attempt will be repeated every frame.
 	// Add an entry with the initial ref count.
-	entries.insert( { key, { handle, 1 } } );
-
-	return handle;
+	entries.insert( { key, { { handles[0], handles[1] }, 1 } } );
+	// Note: return an address of a persistent due to UI lifetime data
+	return entries[key].handles;
 }
 
-void AntiAliasingBitmapCache::ReleaseTexture( const Vector2i &dimensions, float slantAngle ) {
+void AntiAliasingBitmapCache::ReleaseTextures( const Vector2i &dimensions, float slantAngle ) {
 	auto iterator = entries.find( MakeKey( dimensions, slantAngle ) );
 	if( iterator != entries.end() ) {
 		auto &entry = ( *iterator ).second;
 		if( entry.numRefs > 1 ) {
 			entry.numRefs--;
 		} else {
-			if( entry.handle ) {
-				renderInterface->ReleaseTexture( entry.handle );
+			for( int i = 0; i < 2; ++i ) {
+				if( entry.handles[i] ) {
+					renderInterface->ReleaseTexture( entry.handles[i] );
+				}
 			}
 			entries.erase( iterator );
 		}
